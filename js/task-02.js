16e6af4d0cb6296d27220f514a52559b0c70fa73
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2022-11-22 02:20:11 +0500

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsEl = document.querySelector("#ingredients");
+
+const items = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const item = document.createElement("li");
+  item.classList.add("item");
+  item.textContent = ingredients[i];
+  items.push(item);
+}
+ingredientsEl.append(...items);
