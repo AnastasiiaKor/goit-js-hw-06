0dec10d902bea4a307736bafd86a9b76b851672c
Anastasiya Kormilitsina <anastasiya.kormilitsina@gmail.com>
2022-11-23 23:54:51 +0500

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (!email.value || !password.value) {
+    alert("Please, fill in all the fields!");
+  } else {
+    const formFields = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(formFields);
+    event.currentTarget.reset();
+  }
+}
